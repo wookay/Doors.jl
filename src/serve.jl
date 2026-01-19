9adaca012a81803cab790e8e3273e7a7d2f8fc6a
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-20 07:51:24 +0900

    fix `serve()`

--- src/serve.jl
+++ src/serve.jl
@@ -1,7 +1,7 @@
 # module Doors
 
 function serve(port::Union{typeof(any), Integer} = PORT)
-    app = create_app(port)
+    app = create_app(; port)
     !isinteractive() && wait(app.close_notify)
 end
 
