3b8616ff8e75aafa009b7da568ca86c8e8e3ec4f
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-31 18:51:37 +0900

    JiveExt Jive.showable_stackframe

--- ext/JiveExt.jl
+++ ext/JiveExt.jl
@@ -1,27 +1,33 @@
 module JiveExt
 
-using Base.StackTraces: StackFrame
+if VERSION >= v"1.11"
 using Jive
 using Doors
 using IOCapture
 
-import Jive: check_to_hide_the_stackframe
-function check_to_hide_the_stackframe(frame::StackFrame)::Bool
-    HIDE_STACKFRAME_IN_MODULES = [Jive, Doors, IOCapture, Base.Filesystem, Base.CoreLogging]
+HIDE_STACKFRAME_IN_MODULES = Set([Jive, Doors, IOCapture, Base.Filesystem, Base.CoreLogging])
+
+# from Jive.jl/src/errorshow.jl
+import Jive: showable_stackframe
+function showable_stackframe(frame::Base.StackTraces.StackFrame)::Bool
     if Base.parentmodule(frame) in HIDE_STACKFRAME_IN_MODULES
-        return true
+        return false
     elseif frame.func === Symbol("macro expansion")
         target_macro_expansions::Set{String} = Set([
+            "Jive/src/runtests.jl",
+          # "Jive/src/compat.jl",
             "Test/src/Test.jl",
         ])
         frame_file = String(frame.file)
         for suffix in target_macro_expansions
-            endswith(frame_file, suffix) && return true
+            endswith(frame_file, suffix) && return false
         end
     elseif frame.func === :include && frame.file === Symbol("./Base.jl")
-        return true
+        return false
     end
-    return false
-end
+    return true
+end # function showable_stackframe
+
+end # if VERSION >= v"1.11"
 
 end # module JiveExt
