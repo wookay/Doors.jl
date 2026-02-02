a72f275c1a30ac8dd26b818f5ec447cf3755e74f
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-03 08:35:17 +0900

    add `Test` to `HIDE_STACKFRAME_IN_MODULES` in JiveExt.jl

--- ext/JiveExt.jl
+++ ext/JiveExt.jl
@@ -1,10 +1,11 @@
 module JiveExt
 
 if VERSION >= v"1.11"
-using Jive
 using Doors
+using Jive
+using .Jive: Test
 
-HIDE_STACKFRAME_IN_MODULES = Set([Jive, Doors, Base.Filesystem, Base.CoreLogging])
+HIDE_STACKFRAME_IN_MODULES = Set([Doors, Jive, Test, Base.Filesystem, Base.CoreLogging])
 
 # from Jive.jl/src/errorshow.jl
 import Jive: showable_stackframe
@@ -26,6 +27,16 @@ function showable_stackframe(frame::Base.StackTraces.StackFrame)::Bool
     end
     return true
 end # function showable_stackframe
+
+#=
+# from Jive.jl/src/compat.jl
+import Jive: jive_print_testset_verbose
+function jive_print_testset_verbose(action::Symbol, ts::Test.AbstractTestSet)
+    if action === :enter
+        # @info ts.description
+    end
+end
+=#
 
 end # if VERSION >= v"1.11"
 
