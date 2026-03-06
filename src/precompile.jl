2e80052e4b6886bda57a471d3e16955f336c6c3e
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-06 20:31:31 +0900

    precompile Base

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,6 @@
 # module Doors
 
+# Doors
 precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:into,), Tuple{Module}}, Type{Doors.App}})
 precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:into,), Tuple{Module}}, typeof(Doors.serve), Int64})
 precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:port, :into), Tuple{typeof(Base.any), Module}}, typeof(Doors.create_app)})
@@ -9,5 +10,22 @@ precompile(Tuple{typeof(Doors.runexpr), String, UInt16})
 precompile(Tuple{typeof(Doors.runexpr), Expr, UInt16})
 precompile(Tuple{typeof(Doors.runargs), Int64})
 precompile(Tuple{typeof(Doors.shutdown), Doors.App})
+
+# Base
+precompile(Tuple{typeof(Base.getindex), Base.Threads.Atomic{Int32}})
+precompile(Tuple{typeof(Base.:(>=)), Int32, Int32})
+precompile(Tuple{typeof(Base.CoreLogging.shouldlog), Base.CoreLogging.ConsoleLogger, Base.CoreLogging.LogLevel, Module, Symbol, Symbol})
+precompile(Tuple{Type{Base.IOContext{IO_t} where IO_t<:IO}, Base.GenericIOBuffer{Memory{UInt8}}, Base.TTY})
+precompile(Tuple{typeof(Base.getproperty), Base.GenericCondition{Base.ReentrantLock}, Symbol})
+precompile(Tuple{typeof(Base.lock), Base.GenericCondition{Base.Threads.SpinLock}})
+precompile(Tuple{typeof(Base.notify), Base.GenericCondition{Base.Threads.SpinLock}})
+precompile(Tuple{typeof(Base.unlock), Base.GenericCondition{Base.Threads.SpinLock}})
+precompile(Tuple{typeof(Base.push!), Array{String, 1}})
+precompile(Tuple{typeof(Base.write), Base.TTY, Array{UInt8, 1}})
+precompile(Tuple{typeof(Base.lock), Base.TTY})
+precompile(Tuple{typeof(Base.unlock), Base.TTY})
+precompile(Tuple{typeof(Base.ocachefile_from_cachefile), String})
+precompile(Tuple{typeof(Base.close), Base.IOContext{Base.PipeEndpoint}})
+precompile(Tuple{typeof(Base._atexit), Int32}) # recompile
 
 # module Doors
